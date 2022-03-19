ebbec2b27bfb3917797c7078d2fabe8e24c27362
Stephen An <anyang01@qq.com>
2022-03-19 22:30:48 +0800

    parent 4ab766a7a8d73e8d8accdeb77b291f08c34d9348 author Stephen An <anyang01@qq.com> 1647700248 +0800 committer Stephen An <anyang01@qq.com> 1648377145 +0800
    初始化导入，美化控制页面
    
    初始化导入，美化控制页面
    
    更新了图标，变成一只可爱的熊
    
    改了一下按钮文字
    
    添加保存默认主题功能
    
    user jquery 3.6 and redesign the layout
    
    Update

---
+++ js/popup.js
@@ -0,0 +1,69 @@
+﻿function getCurrentTabId(callback)
+{
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs)
+	{
+		if(callback) callback(tabs.length ? tabs[0].id: null);
+	});
+}
+
+
+function sendMessageToContentScript(message, callback)
+{
+	getCurrentTabId((tabId) =>
+	{
+		chrome.tabs.sendMessage(tabId, message, function(response)
+		{
+			if(callback) callback(response);
+		});
+	});
+}
+
+$('#btCode').on('click',() => {
+	sendMessageToContentScript({cmd:'generate', value:'Hello Stephen！'}, (response) => {
+		if(response) {
+            var result = JSON.parse(response);
+            $('#taCode').val(result.value);
+            // showWindowsTip(result);
+        }
+	});
+});
+
+$('#btReminder').on('click',() => {
+	sendMessageToContentScript({cmd:'reminder', value:'Hello Stephen！'}, (response) => {
+		if(response) {
+            var result = JSON.parse(response);
+            $('#taCode').val(result.value);
+            // showWindowsTip(result);
+        }
+	});
+});
+
+function showWindowsTip(response){
+    chrome.notifications.create(null, {
+        type: 'basic',
+        iconUrl: 'icon.png',
+        title: response.cmd,
+        message: response.value
+    });
+}
+
+
+$('#theme').on('click',() => {
+	var themeVal = $('body').attr('data-theme');
+	if (themeVal == 'light') {
+		themeVal = 'dark';
+	} else {
+		themeVal = 'light';
+	}
+
+	$('body').attr('data-theme',themeVal);
+	chrome.storage.sync.set({theme: themeVal}, function() {
+		console.log('Save default theme '+ themeVal +'successfylly');
+	});
+	
+});
+
+chrome.storage.sync.get({theme: 'dark'}, function(items) {
+	$('body').attr('data-theme',items.theme);
+	console.log('Load default theme '+ items.theme +'successfylly');
+});
