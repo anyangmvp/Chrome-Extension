43d5629a792c1dd339094099fc2bd32b623829b1
Stephen An <anyang01@qq.com>
2022-03-19 22:30:48 +0800

    初始化导入

---
+++ js/content-script.js
@@ -0,0 +1,38 @@
+﻿chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
+{
+	if(request.cmd == 'generate') {
+		tip("Generating Code");
+		var result = document.title;
+		//generate code
+		sendResponse(JSON.stringify({cmd:'generate',value:"Generate Code :\n"  + result}));
+	}else if(request.cmd == 'reminder') {
+		tip("Togger Reminder");
+		//togo reminder
+		$('#Header1_HeaderTitle').toggle();
+		var result = document.location.href;
+		sendResponse(JSON.stringify({cmd:'reminder',value:"Toggle Reminder :\n"  + result}));
+	}
+	else {
+		tip(JSON.stringify(request.value));
+		sendResponse('Got your message：'+JSON.stringify(request));
+	}
+});
+
+var tipCount = 0;
+function tip(info) {
+	info = info || '';
+	var ele = document.createElement('div');
+	ele.className = 'chrome-plugin-simple-tip slideInLeft';
+	ele.style.top = tipCount * 70 + 20 + 'px';
+	ele.innerHTML = `<div>${info}</div>`;
+	document.body.appendChild(ele);
+	ele.classList.add('animated');
+	tipCount++;
+	setTimeout(() => {
+		ele.style.top = '-100px';
+		setTimeout(() => {
+			ele.remove();
+			tipCount--;
+		}, 400);
+	}, 3000);
+}
---
+++ js/popup.js
@@ -0,0 +1,48 @@
+﻿function getCurrentTabId(callback)
+{
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs)
+	{
+		if(callback) callback(tabs.length ? tabs[0].id: null);
+	});
+}
+
+
+function sendMessageToContentScript(message, callback)
+{
+	getCurrentTabId((tabId) =>
+	{
+		chrome.tabs.sendMessage(tabId, message, function(response)
+		{
+			if(callback) callback(response);
+		});
+	});
+}
+
+$('#btCode').click(() => {
+	sendMessageToContentScript({cmd:'generate', value:'Hello Stephen！'}, (response) => {
+		if(response) {
+            var result = JSON.parse(response);
+            $('#taCode').val(result.value);
+           // showWindowsTip(result);
+        }
+	});
+});
+
+$('#btReminder').click(() => {
+	sendMessageToContentScript({cmd:'reminder', value:'Hello Stephen！'}, (response) => {
+		if(response) {
+            var result = JSON.parse(response);
+            $('#taCode').val(result.value);
+           // showWindowsTip(result);
+        }
+	});
+});
+
+function showWindowsTip(response){
+    chrome.notifications.create(null, {
+        type: 'basic',
+        iconUrl: 'icon.png',
+        title: response.cmd,
+        message: response.value
+    });
+}
